d218d684a2486112a8d5b3cfad6198965ff3c727
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-05-29 22:20:08 +0400

    Added hero bg image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ const Hero = () => {
     return(
         <div className="container pt-64">
             <div className="row">
-                <div className="col-12 col-lg-4 bg-primary">text</div>
-                <div className="col-12 col-lg-8 bg-primary">
-                    <div className='bg-danger text-end'>
-                        <img src={HeroBg} alt="" className='w-100' />
+                <div className="col-12 col-lg-4 bg-primary order-2 order-lg-1">text</div>
+                <div className="col-12 col-lg-8 order-1 order-lg-2">
+                    <div className='text-lg-end text-center'>
+                        <img src={HeroBg} alt="" className='img-fluid' />
                     </div>
                 </div>
             </div>
