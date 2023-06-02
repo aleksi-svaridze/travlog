026151934610395b40f92cab4d66b58986b2b4a5
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-06-02 10:10:52 +0400

    Added wavelines bg to Newsletter section

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,9 +1,11 @@
 import Dots from '../images/icons/dots_orange.svg';
+import WaveLines from '../images/icons/wave-lines.svg';
 
 const Newsletter = () => {
     return(
         <div className="container py-rs-128 px-rs-64 bg-yellow rounded-5 position-relative mt-lg-177">
             <img src={Dots} alt="" className='w-184 h-176 position-absolute start-n35 top-n67 d-none d-lg-block'/>
+            <img src={WaveLines} alt="" className='position-absolute end-n348 bottom-n222 d-none d-lg-block' />
              <p className="text-pink text-uppercase fs-4 mb-32 text-center" style={{letterSpacing: '.2em'}}>subscribe to our newsletter</p>
             <h2 className="text-blue fs-lg-55 fs-32 text-center mb-64">Prepare yourself & let's explore the beauty of the world</h2>
         </div>
