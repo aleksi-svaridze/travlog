7ee1e773009a8946f3aa209cde3d3f7d5f8b5b2b
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-06-02 12:27:29 +0400

    Added: Envelope icon to Newsletter section

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -3,14 +3,14 @@ import WaveLines from '../images/icons/wave-lines.svg';
 
 const Newsletter = () => {
     return(
-        <div className="container py-rs-128 px-rs-64 bg-yellow rounded-5 position-relative mt-lg-177">
+        <div className="container py-rs-128 px-rs-64 bg-yellow-10 rounded-5 position-relative mt-lg-177 mb-rs-64">
             <img src={Dots} alt="" className='w-184 h-176 position-absolute start-n35 top-n67 d-none d-lg-block'/>
             <img src={WaveLines} alt="" className='position-absolute end-n348 bottom-n222 d-none d-lg-block' />
              <p className="text-pink text-uppercase fs-4 mb-32 text-center" style={{letterSpacing: '.2em'}}>subscribe to our newsletter</p>
             <h2 className="text-blue fs-lg-55 fs-32 text-center mb-64">Prepare yourself & let's explore the beauty of the world</h2>
 
             <form className='d-flex flex-column row-gap-32 flex-lg-row column-gap-lg-64 justify-content-lg-between'>
-                <input type="text" className="form-control fs-lg-24 fs-sm-14 text-blue-dark-75 rounded-lg-32 rounded-16 h-67 h-lg-96 p-4 p-lg-32" placeholder="your email" />
+                <input type="text" className="form-control ps-rs-72 bs-rs-32 fs-lg-24 fs-sm-14 text-blue-dark-75 rounded-lg-32 rounded-16 h-67 h-lg-96 p-4 p-lg-32 border-0" placeholder="your email" />
                 <button className='btn text-capitalize fs-lg-24 h-67 h-lg-96 w-lg-235 bg-purple rounded-lg-32 rounded-16 border-0 text-white'>subscribe</button>
             </form>
         </div>
